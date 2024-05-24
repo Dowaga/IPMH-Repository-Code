8e280b90826af91e67def7d947c81557f2a4fef0
David Owaga <owaga2007@gmail.com>
2024-05-25 01:08:52 +0300

    Delete HCWs Demo

--- Test.R
+++ Test.R
@@ -3,6 +3,3 @@ rm(list = ls())
 pacman::p_load(knitr, tidyverse, ggplot2, psych, janitor, dplyr, 
                openxlsx, gtsummary, kableExtra) 
 
-## Health Care Workers Demographics
-
-We are reporting 6 Focus Group Discussions (FGDs) for health care workers, with 4 comprising 7 participants each and 2 comprising 6 participants each - altogether **40 participants**. 
