75c0eaa06fd3ea22015d83bd9726206b9ba97e07
David Owaga <owaga2007@gmail.com>
2024-07-12 11:48:53 +0300

    Updated OneDrive data folder

--- DataTeam_ipmh.R
+++ DataTeam_ipmh.R
@@ -3,14 +3,14 @@
 da_users <- data.frame(Analyst = c("Owaga", "Lincoln Pothan" ),
                        github_user = c("Dowaga", "lpothan"),
                        git_filepath = c("C:/Program Files/Git/", "/usr/local/bin/git"),
-                       ipmh_filepath = c("C:/Users/Damaris/uw/ngumbau-IPMH", "/Users/Lincolnpothan/Library/CloudStorage/OneDrive-UW/IPMH study"),
+                       ipmh_filepath = c("C:/Users/Damaris/uw/ngumbau - IPMH study", "/Users/Lincolnpothan/Library/CloudStorage/OneDrive-UW/IPMH study"),
                        machinetype = c("PC", "Mac"))
 
 ### Defining OneDrive data filepath based on analyst ###
 current_wd <- getwd()
 
 if (grepl("Dowaga", current_wd)) {
-  ipmh_filepath <- "C:/Users/Damaris/uw/ngumbau-IPMH"
+  ipmh_filepath <- "C:/Users/Damaris/uw/ngumbau - IPMH study"
 } else if (grepl("Lincolnpothan", current_wd)) {
     ipmh_filepath <- "/Users/Lincolnpothan/Library/CloudStorage/OneDrive-UW/IPMH study"
 } else {
@@ -19,7 +19,7 @@ if (grepl("Dowaga", current_wd)) {
 
 ### Working Directory Check ###
 
-if(endsWith(current_wd, "IPMH-Repository-Code")) {ipmh_wd <- current_wd
+if(endsWith(current_wd, "IPMH Repository Code")) {ipmh_wd <- current_wd
 } else {
   print("WARNING: Incorrect Working Directory")
 } 
