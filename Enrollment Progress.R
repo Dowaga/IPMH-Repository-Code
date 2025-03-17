5a4e7edb8d590c3fa3c823b84b88f96f35505ba6
David Owaga <owaga2007@gmail.com>
2025-03-17 21:01:13 +0300

    Reducing Tension Checlists

--- Enrollment Progress.R
+++ Enrollment Progress.R
@@ -9,7 +9,12 @@ source("DataTeam_ipmh.R")
 source("Dependencies.R")
 source("data_import.R")
 
+#### Setting plot themes to format graphs
+theme_set(theme_bw())
+theme_update(plot.title = element_text(hjust = 0.5))
 #--------------------------------------------------------------------------
+
+
 ## Figure 1: Enrollment progress since the beginning of the study to today
 enrollment_progress <- screening_consent_df %>% 
     filter(rct_enrolling == "Yes") %>% 
@@ -26,7 +31,7 @@ weekly_count <- enrollment_progress %>%
     summarise(enrollment_count = n(), .groups = "drop")  # Count enrollments
 
 # Define the sequence of weekly dates (assuming enrollment started on 2025-02-17)
-date_seq <- seq(as.Date("2025-02-17"), as.Date("2025-03-14"), by = "week")
+date_seq <- seq(as.Date("2025-02-16"), as.Date("2025-03-14"), by = "week")
 
 # Convert to a dataframe
 dateSeq_df <- data.frame(week = date_seq)
@@ -44,7 +49,7 @@ weekly_enrollment <- weekly_enrollment %>%
     ungroup()
 
 
-ggplot(weekly_enrollment, aes(x = week, y = cumulative_enrollment, color = study_site, group = study_site)) +
+figure_1 <- ggplot(weekly_enrollment, aes(x = week, y = cumulative_enrollment, color = study_site, group = study_site)) +
     geom_line(size = 1) +  # Line plot for trends
     geom_point(size = 2) +  # Add points for clarity
     labs(title = "Weekly Enrollment Trends by Study Site",
@@ -54,4 +59,25 @@ ggplot(weekly_enrollment, aes(x = week, y = cumulative_enrollment, color = study
     theme_minimal() +
     theme(legend.position = "right")  # Adjust legend placement
 
+figure_1
 
+## Weekly enrollment table by Facility
+Enrollment_wide <- weekly_enrollment %>%
+    select(study_site, week, enrollment_count) %>% 
+    pivot_wider(
+        names_from = week,          # Convert weeks into column names
+        values_from = enrollment_count,  # Fill values with enrollment counts
+        values_fill = list(enrollment_count = 0)  # Fill missing values with 0
+    ) %>% 
+    adorn_totals(where = c("row","col")) %>% 
+    arrange(desc(Total))
+pacman::p_load(dplyr, tidyr, janitor, flextable)
+
+# Convert to flextable and format totals
+Enrollment_wide_ft <- Enrollment_wide %>%
+    flextable() %>%
+        bold(part = "body", i = 1) %>%  # Bold column total
+    italic(part = "body", i = 1) %>%  # Italicize column total
+    autofit()  # Adjust table to fit content
+
+Enrollment_wide_ft
