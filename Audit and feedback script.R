5c5d0eb8957cb696411a403660269f9b21ced267
David Owaga <owaga2007@gmail.com>
2025-03-26 14:25:02 +0300

    GAD7 codes

--- Audit and feedback script.R
+++ Audit and feedback script.R
@@ -252,7 +252,7 @@ rct_ppw_int <- rct_ppw_int %>%
                   ~ case_when(
                       . == "Not at all" ~ 0,
                       . == "Several days" ~ 1,
-                      . == "More than half the days" ~ 2,
+                      . == "Over half the days" ~ 2,
                       . == "Nearly every day" ~ 3,
                       TRUE ~ NA_real_
                   ), 
