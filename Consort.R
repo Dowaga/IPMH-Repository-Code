0e6d56cccd377710d1c47781430bba999a011de5
David Owaga <owaga2007@gmail.com>
2025-04-04 11:30:22 +0300

    ANC attendees

--- Consort.R
+++ Consort.R
@@ -11,15 +11,23 @@ source("DataTeam_ipmh.R")
 source("Dependencies.R")
 source("data_import.R")
 
-Attendees <- daily_closeout_df %>% 
-    summarise(`ANC Attendees` = sum(rct_anc_number))
+# Summarise ANC attendees and extract the sum as a numeric value
+Attendees <- daily_closeout_df %>%
+    summarise(`anc attendees` = sum(rct_anc_number, na.rm = TRUE)) %>%
+    pull(`annnc attendees`)  # Extract the sum value as a number
+
+# Create a new data frame with anc_count rows, value = "Yes"
+anc_attendees_df <- data.frame(
+    anc_id = paste0("anc_", seq_len(Attendees)),
+    anc_attendees = rep("Yes", Attendees)
+)
 
 pm_abstractions <- pm_survey_df %>% 
     select(record_id, ipmh_participant) %>% 
     filter(ipmh_participant == "Yes")
 
 consort_data <- screening_consent_df %>% 
-    select(record_id, rct_harm_thought, rct_aud_hallucinations, 
+    select(record_id, partipant_id, rct_harm_thought, rct_aud_hallucinations, 
            rct_vis_hallucinations, rct_paranoia,rct_delusions,
            rct_memory_problem,rct_eligible_gestation, rct_eligible_harm, 
            rct_eligible, rct_enrolling, rct_decline_reason, 
@@ -36,10 +44,17 @@ consort_data <- screening_consent_df %>%
         rct_memory_problem == "Yes" ~"Memory problem",
         rct_delusions == "Yes" ~ "Holding unusual beliefs",
         TRUE ~ NA_character_)) %>% 
+    mutate(facility = as.numeric(str_sub(partipant_id, 3, 4))) %>% 
+    mutate(arm = case_when(
+        facility %in% c(2, 5, 6, 8, 11, 14, 15, 18, 20, 21) ~ "Control",
+        TRUE ~ "Intervention"
+    )) %>% 
     mutate(rct_decline_reason = case_when(
             rct_decline_reason == "Other (specify) ___" ~ "Relocate post delivery",
             TRUE ~ rct_decline_reason  # Keep other values unchanged
-        ))
+        )) 
+
+consort_data <- bind_rows(anc_attendees_df, consort_data)
 
 elig <- consort_data %>% 
     filter(is.na(eligible)) %>% 
@@ -49,13 +64,17 @@ decline_reason <- consort_data %>%
     filter(!is.na(rct_decline_reason))
 
 consort_diagram <- consort_plot(data = consort_data,
-                    orders = c(record_id = "Assessed for Eligibility ",
+                    orders = c(anc_attendees = "ANC Attendees",
+                        record_id = "Assessed for Eligibility ",
                                exclusion = "Excluded",
                                eligible = "Eligible",
                                rct_decline_reason = "Declined Enrollment",
-                               rct_enrolling = "Enrolled"),
+                               rct_enrolling = "Enrolled",
+                               arm = "Arm"),
                     side_box = c("exclusion", "rct_decline_reason"),
-                    cex = 0.9)
+                    allocation = "arm",
+                    cex = 0.7)
+
 
 
 
