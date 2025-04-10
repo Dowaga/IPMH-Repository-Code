d2446e6d6aa0fc5f3ea8d9123ad93e637284fa0d
David Owaga <owaga2007@gmail.com>
2025-04-10 18:17:25 +0300

    Facility yields

--- Study Staffs PHQ9 and GAD7 Screening Yield.R
+++ Study Staffs PHQ9 and GAD7 Screening Yield.R
@@ -10,13 +10,96 @@ source("Dependencies.R")
 source("data_import.R")
 #--------------------------------------------------------------------------
 
-# Intervention Screening Yield for Facility administred Survey
-yield_df <- ppw_rct_df %>% 
-    filter(redcap_event_name == "Enrollment (Arm 1: Intervention)")
+# Intervention Facility Staff Screening Yields (PHQ9 and GAD7)
+facility_yield_df <- ppw_rct_df %>% 
+    filter(redcap_event_name == "Enrollment (Arm 1: Intervention)") %>% 
+    select(record_id, clt_study_site,starts_with("abs_"))
+
+# Define PHQ9 recoding
+phq9_labels <- c(
+    "not at all" = 0,
+    "several days" = 1,
+    "more than half the days" = 2,
+    "nearly every day" = 3
+)
+
+gad7_labels <-c(
+    "Not at all" = 0,
+    "Several days" = 1,
+    "Over half the days"= 2,
+    "Nearly every day" = 3)
+
+
+# Recode PHQ9 variables
+facility_yield_df <- facility_yield_df %>%
+    mutate(across(c(abs_phq_interest, abs_phq_down, abs_phq_sleep,
+                    abs_phq_tired, abs_phq_appetite, abs_phq_bad,
+                    abs_phq_concentrate, abs_phq_slow, abs_phq_dead), ~ recode(., !!!phq9_labels)),
+           across(c(abs_gad7_nerve, abs_gad7_uncontrol, 
+                    abs_gad7_worry, abs_gad7_relax, abs_gad7_restless,
+                    abs_gad7_annoyed, abs_gad7_afraid), ~ recode(., !!!gad7_labels)))
 
 
 
-# Study-administered Screening Yield
+facility_yield_df <- facility_yield_df %>% 
+    mutate(phq9_scores = rowSums(select(., abs_phq_interest, abs_phq_down,
+                                        abs_phq_sleep, abs_phq_tired, 
+                                        abs_phq_appetite, abs_phq_bad,
+                                        abs_phq_concentrate, abs_phq_slow, 
+                                        abs_phq_dead), na.rm = TRUE),
+           gad7_scores = rowSums(select(., abs_gad7_nerve, abs_gad7_uncontrol,
+                                        abs_gad7_worry, abs_gad7_relax, 
+                                        abs_gad7_restless, abs_gad7_annoyed,
+                                        abs_gad7_afraid), na.rm = TRUE))
+
+# Endorsed PHQ9 Question 9
+self_harm <- facility_yield_df %>% 
+    filter(abs_phq_dead > 0)
+
+
+# PHQ9 And GAD7 Positives
+facility_yield <- facility_yield_df %>%
+    mutate(
+        phq9_positive = if_else(phq9_scores >= 10, 1, 0),  # common cutoff is ???10
+        gad7_positive = if_else(gad7_scores >= 10, 1, 0)
+    ) %>% 
+    mutate(clt_study_site = gsub("^[0-9]+,\\s*", "", clt_study_site)) %>% 
+    mutate
+
+fc_yield_summary <- facility_yield %>%
+    group_by(clt_study_site) %>%
+    summarise(
+        screened_phq9 = sum(!is.na(phq9_scores)),
+        #Overall = sum(phq9_positive | gad7_positive, na.rm = TRUE),
+        phq9_positive = sum(phq9_positive, na.rm = TRUE),
+        screened_gad7 = sum(!is.na(gad7_scores)),
+        gad7_positive = sum(gad7_positive, na.rm = TRUE)
+    ) %>%
+    mutate(
+        `PHQ9 Yield` = round((phq9_positive / screened_phq9) * 100, 1),
+        `GAD7 Yield` = round((gad7_positive / screened_gad7) * 100, 1)
+    ) %>% 
+    select(-c(screened_gad7)) %>% 
+    rename(`Total Screened` = screened_phq9,
+           `PHQ9 Positive` = phq9_positive,
+           `GAD7 Positive` = gad7_positive,
+           `Facility` = clt_study_site) %>% 
+    # convert from gtsummary object to gt object
+    gt() %>%
+    # modify with gt functions
+    gt::tab_header(
+        title = "Table 3: Facility Administered PHQ9 and GAD7 Screening Yields",
+        subtitle = "Showing Facility Staff PHQ9 and GAD& Yieds") %>%
+    gt::tab_options(
+        table.font.size = "medium",
+        data_row.padding = gt::px(1)) %>%
+    tab_options(
+        table.font.size = px(14))
+
+fc_yield_summary
+
+#--------------------------------------------------------------------------
+# Study Staff Screening Yields(PHQ9 and GAD7)
 study_yield_df <- ppw_rct_df %>% 
         select(record_id, clt_study_site, starts_with("phq_"), starts_with("gad7_"))
 
@@ -62,7 +145,6 @@ self_harm <- study_yield_df %>%
     filter(phq_dead > 0)
 
 study_yield_df <- study_yield_df %>% 
-    #filter((phq9_scores >= 10)|(gad7_scores >= 10)) %>% 
     mutate(
         max_score = pmax(phq9_scores, gad7_scores, na.rm = TRUE),  # Get the greatest score
         eligible_for = case_when(
@@ -70,7 +152,7 @@ study_yield_df <- study_yield_df %>%
                  ((max_score == phq9_scores)|(max_score == gad7_scores))) ~ "PM+",
             max_score >= 15 ~ "Telepsychiatry"))
 
-# Assuming your data frame is called `df`
+# PHQ9 And GAD7 Positives
 yield_df <- study_yield_df %>%
     select(-c(phq_difficulty, gad7_difficulty)) %>% 
     mutate(
@@ -115,4 +197,6 @@ yield_summary <- yield_df %>%
         table.font.size = px(14))
 
 yield_summary
+
+
 
