379311b9832f14eda47a7b637792e698b71888a4
David Owaga <owaga2007@gmail.com>
2025-05-05 14:05:35 +0300

    update of brakes

--- Consort.R
+++ Consort.R
@@ -140,25 +140,13 @@ txt_decline_intervention <- "Declined Enrollment (n=13, 4.25%)\n\u2022 Not enoug
 txt_decline <- c(txt_decline_control, txt_decline_intervention)
 txt_enrol <- c("Enrolled (n=340, 94.44%)", "Enrolled (n=293, 95.75%)")
 
-=======
-txt_anc <- c("ANC Attendees (n=4,089)")
-txt_ass <- c("Assessed for Eligibility\n (n=1,018, 24.90%)") 
-txt_arm <- c("Control\n (n=527, 51.77%)", "Intervention\n (n=491, 48.23%)")
-txt_ex_control <- "Excluded (n=194, 36.81%):\n\u2022 Gestation < 28 weeks (n=177, 91.24%)\n\u2022 Hearing voices that others cannot hear (n=3, 1.55%)\n\u2022 Holding unusual beliefs (n=4, 2.06%)\n\u2022 Memory problem (n=3, 1.55%)\n\u2022 Self harm (n=5, 2.58%)\n\u2022 Self harm and memory problem (n=1, 0.52%)"
-txt_ex_intervention <- "Excluded (n=203, 41.34%):\n\u2022 Gestation < 28 weeks (n=198, 97.54%)\n\u2022 Memory problem (n=1, 0.49%)\n\u2022 Self harm (n=4, 1.97%)"
-txt_ex <- c(txt_ex_control, txt_ex_intervention)
-txt_eli <- c("Eligible\n (n=332, 63.00%)", "Eligible\n (n=288, 58.66%)")
-txt_decline_control <- "Declined Enrollment (n=17, 5.12%)\n\u2022 Consult spouse (n=2, 11.76%)\n\u2022 Not enough time (n=3, 17.65%)\n\u2022 Not interested (n=3, 17.65%)\n\u2022 Relocate post delivery (n=6, 35.29%)\n\u2022 Time to think about it (n=3, 17.65%)"
-txt_decline_intervention <- "Declined Enrollment (n=12, 4.17%)\n\u2022 Not enough time (n=5, 41.67%)\n\u2022 Relocate post delivery (n=7, 58.33%)"
-txt_decline <- c(txt_decline_control, txt_decline_intervention)
-txt_enrol <- c("Enrolled (n=340, 94.88%)", "Enrolled (n=276, 95.83%)")
-               
->>>>>>> 3726121ecb0f3c621a7bf2a3a4198199f4de42db
+      
+##### 3726121ecb0f3c621a7bf2a3a4198199f4de42db
                
 txt_pm <- c("Study Nurse PM+ Yields\n (n=0, 0%)", 
             "Study Nurse PM+ Yields\n (n=65, 23.55%)")
 
-=======
+#####
 #automatically adding percentages (for weekly reports) ----------------
 # Total ANC attendees
 n_attendees <- consort_data %>% 
