8bae7b7f404f3c9cde7c11607a40319cb6185009
David Owaga <owaga2007@gmail.com>
2025-05-09 11:41:26 +0300

    wlwh demo

--- PPW RCT Basic Demographics.R
+++ PPW RCT Basic Demographics.R
@@ -14,7 +14,8 @@ source("data_import.R")
 # data prep --------------------------------------------------------------------
 demographics_df <- ppw_rct_df %>% 
     filter(clt_visit == "Enrollment") %>% 
-    select(clt_date,starts_with("dem_"), demographics_complete, med_pre_preg)%>% 
+    select(clt_date,starts_with("dem_"), demographics_complete, med_pre_preg, 
+           med_pastdiag___2)%>% 
     mutate(dem_age = if_else(
         dem_dob_uk == "Yes",
         floor(time_length(interval(dem_dob, clt_date), "years")),
@@ -35,7 +36,7 @@ demographics_df <- ppw_rct_df %>%
 
 
 # basic demo table
-basic_demo <- demographics_df %>% 
+basic_demo <- demographics_df %>%
     tbl_summary(
     sort = list(all_categorical() ~ "frequency"),  # Sort categorical levels by frequency in descending order
     include=c(dem_age, dem_current_partner, dem_maritalstat,
@@ -77,3 +78,50 @@ basic_demo <- demographics_df %>%
         table.font.size = px(14))
 
 basic_demo
+
+# WLWH Cohort Baseline Demographics
+# basic demo table
+wlwh_demo <- demographics_df %>%
+    filter(med_pastdiag___2 == "Checked") %>% 
+    tbl_summary(
+        sort = list(all_categorical() ~ "frequency"),  # Sort categorical levels by frequency in descending order
+        include=c(dem_age, dem_current_partner, dem_maritalstat,
+                  dem_marriage, dem_pc_residence, dem_current_school, dem_school, 
+                  dem_employment, dem_household_num, dem_housesleep, 
+                  dem_houserooms,dem_traveltime_min, med_pre_preg),
+        label = list(dem_age ~ "Age (Years)",
+                     dem_current_partner ~ "Do you currently have a partner (Yes)",
+                     dem_employment ~ "Do you have regular employment (Yes)",
+                     dem_maritalstat ~ "Currently Married (Yes)",
+                     dem_pc_residence ~ "Shares residence with partner (Yes)",
+                     dem_marriage ~ "Marriage(Monogamous)",
+                     dem_current_school ~ "Currently in School",
+                     dem_school ~ "Completed years in School",
+                     dem_household_num ~ "Number of people in Household",
+                     dem_housesleep ~ "Number of people sleep in same house",
+                     dem_houserooms ~ "Rooms in the house most often sleep in",
+                     dem_traveltime_min ~ "Time to the Clinic (Minutes",
+                     med_pre_preg ~ "Have you been pregnant before(Yes)"),
+        missing = "no",
+        digits = list(all_continuous() ~ 1), 
+        type = list(dem_age ~ "continuous", 
+                    dem_household_num ~ "continuous",
+                    dem_housesleep ~ "continuous", 
+                    dem_houserooms ~ "continuous", 
+                    dem_traveltime_min ~ "continuous"),
+        statistic = list(all_continuous() ~ "{median} [IQR: {p25}, {p75}]")) %>% 
+    bold_labels() %>%
+    add_n() %>% 
+    #add_p() %>% 
+    # convert from gtsummary object to gt object
+    as_gt() %>%
+    # modify with gt functions
+    gt::tab_header("WLWH Demographic Summary") %>% 
+    gt::tab_options(
+        table.font.size = "medium",
+        data_row.padding = gt::px(1)) %>%
+    tab_options(
+        table.font.size = px(14))
+
+wlwh_demo
+
