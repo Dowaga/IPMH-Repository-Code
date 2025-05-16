d4017730da50eb1b0594e00fb48176f8844522b2
David Owaga <owaga2007@gmail.com>
2025-05-16 17:38:20 +0300

    SAEs

---
+++ SAEs Lists.R
@@ -0,0 +1,29 @@
+# Header ------------------------------------------------------------------
+
+# Author(s): Owaga
+# Date: March 11, 2025
+# AEs Lists
+
+# Setup ------------------------------------------------------------------------
+
+# Reference source codes & other dependencies:
+source("DataTeam_ipmh.R")
+source("Dependencies.R")
+source("data_import.R")
+
+sae_df <- ppw_sae_df %>% 
+    select(record_id, redcap_repeat_instance, starts_with("ae_")) %>% 
+    filter(ae_yn == "Yes" & !str_detect(ae_cat, "SAE"))
+
+sae_report <- sae_df %>% 
+    group_by(ae_cat) %>% 
+    mutate(ae_cat = if_else(record_id == "21031003" & ae_cat == "New/prolonged hospitalization (SAE)",
+                            "Death (Infant or Maternal) (SAE)",
+                            ae_cat)) %>% 
+    rename(Event = ae_cat) %>% 
+    summarise(`Number of Events` = n(),
+              `SAE Reported` = n(),
+              `% Reported` = ifelse(`Number of Events` > 0, round((`Number of Events` / `SAE Reported`)) * 100, 1)) %>% 
+    gt()
+
+sae_report
