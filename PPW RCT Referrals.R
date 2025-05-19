213b454da60189fcd1a34f41905f5abbf66b7eda
David Owaga <owaga2007@gmail.com>
2025-05-19 10:55:01 +0300

    as gt

--- PPW RCT Referrals.R
+++ PPW RCT Referrals.R
@@ -135,21 +135,27 @@ summary_tbl <- df_long %>%
     )
 
 
+# Convert percentages and format table
 referral_summary <- summary_tbl %>%
     mutate(
-        pct_screened = scales::percent(pct_screened / 100, accuracy = 0.1),
-        pct_accepted = scales::percent(pct_accepted / 100, accuracy = 0.1)
+        pct_screened = percent(pct_screened / 100, accuracy = 0.1),
+        pct_accepted = percent(pct_accepted / 100, accuracy = 0.1)
     ) %>%
-    kableExtra::kbl(
-        col.names = c("Referral Condition", "n", "% ", "n", "%"),
-        booktabs = TRUE, digits = 1
+    gt() %>%
+    tab_header(title = "Sub Cohort Referral Summary") %>%
+    cols_label(
+        Referral_Condition = "Referral Condition",
+        n_screened = "n Screened",
+        pct_screened = "% Screened",
+        n_accepted = "n Accepted",
+        pct_accepted = "% Accepted"
     ) %>%
-    kableExtra::add_header_above(
-        c(" " = 1, "Screened" = 2, "Accepted" = 2)
-    ) %>% kableExtra::row_spec(nrow(summary_tbl), bold = TRUE)%>%
-    kableExtra::kable_styling(full_width = FALSE)
+    tab_spanner(label = "Screened", columns = c(n_screened, pct_screened)) %>%
+    tab_spanner(label = "Accepted", columns = c(n_accepted, pct_accepted)) %>%
+    fmt_number(columns = c(n_screened, n_accepted), decimals = 0) %>%
+    fmt_percent(columns = c(pct_screened, pct_accepted), decimals = 1) %>%
+    opt_table_lines()
 
-
+# Display table
 referral_summary
 
-#### By Arm Summary_____________________________________________________________
