c48009bdf6ad4e2bba3ff1dffc77d511b0bb6771
David Owaga <owaga2007@gmail.com>
2025-05-22 13:36:50 +0300

    AEs

--- SAEs Lists.R
+++ SAEs Lists.R
@@ -12,14 +12,15 @@ source("Dependencies.R")
 source("data_import.R")
 
 ae_df <- ppw_sae_df %>% 
-    select(record_id, redcap_repeat_instance, starts_with("ae_")) %>% 
+    select(record_id, redcap_repeat_instance, starts_with("ae_"), 
+           redcap_event_name, redcap_repeat_instance) %>% 
     filter(ae_yn == "Yes" & !str_detect(ae_cat, "SAE"))
 
 sae_df <- ppw_sae_df %>% 
     select(record_id, redcap_repeat_instance, starts_with("ae_")) %>% 
     filter(ae_yn == "Yes" & str_detect(ae_cat, "SAE"))
 
-# SAEs--------------------------------------------------------------------------
+# SAEs Report-------------------------------------------------------------------
 sae_report <- sae_df %>% 
     group_by(ae_cat) %>% 
     mutate(ae_cat = if_else(record_id == "21031003" & ae_cat == "New/prolonged hospitalization (SAE)",
@@ -28,20 +29,21 @@ sae_report <- sae_df %>%
     rename(Event = ae_cat) %>% 
     summarise(`Number of Events` = n(),
               `SAE Reported` = n(),
-              `% Reported` = ifelse(`Number of Events` > 0, round((`Number of Events` / `SAE Reported`)) * 100, 1)) %>% 
+              `% Reported` = ifelse(`Number of Events` > 0, round((`Number of Events` / `SAE Reported`)) * 100, 1)) %>%
     gt()
 
 sae_report
 
+# AE Report---------------------------------------------------------------------
 ae_report <- ae_df %>% 
+    filter(redcap_repeat_instance == 2|!str_detect(redcap_event_name, "Enrollment")) %>% 
     group_by(ae_cat) %>% 
     mutate(ae_cat = if_else(record_id == "21031003" & ae_cat == "New/prolonged hospitalization (SAE)",
-                            "Death (Infant or Maternal) (SAE)",
-                            ae_cat)) %>% 
+                            "Death (Infant or Maternal) (SAE)",ae_cat)) %>% 
     rename(Event = ae_cat) %>% 
-    summarise(`Number of Events` = n(),
-              `SAE Reported` = n(),
-              `% Reported` = ifelse(`Number of Events` > 0, round((`Number of Events` / `SAE Reported`)) * 100, 1)) %>% 
+    summarise(`Number of Events` = n()) %>% 
     gt()
 
 ae_report
+
+
