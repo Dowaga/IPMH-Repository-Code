d2d99951ea6b75dbf708f455417e8acc68c4b085
Olivia Schultes <99046661+oschultes@users.noreply.github.com>
2025-11-10 18:25:01 -0800

    updated file paths for Olivia's computer

--- DataTeam_ipmh.R
+++ DataTeam_ipmh.R
@@ -1,16 +1,18 @@
 ### Data Team Info ###
 
-da_users <- data.frame(Analyst = c("David Owaga", "Lincoln Pothan", "Yuwei Wang", "David OWaga"),
-                       github_user = c("Dowaga", "lpothan", "yuwei-eve", "Dowaga"),
+da_users <- data.frame(Analyst = c("David Owaga", "Lincoln Pothan", "Yuwei Wang", "David OWaga", "Olivia Schultes"),
+                       github_user = c("Dowaga", "lpothan", "yuwei-eve", "Dowaga", "oschultes"),
                        git_filepath = c("C:/Program Files/Git/cmd/git.exe", 
                                         "/usr/local/bin/git", 
                                         "usr/local/bin/git", 
+                                        "C:/Program Files/Git/cmd/git.exe",
                                         "C:/Program Files/Git/cmd/git.exe"),
                        ipmh_filepath = c("C:/Users/DAMARIS/UW/ngumbau - IPMH study", 
                                          "/Users/Lincolnpothan/Library/CloudStorage/OneDrive-UW/IPMH study",
                                          "/Users/yuweiwang_1997/Library/CloudStorage/OneDrive-UW/IPMH study",
-                                         "C:/Users/hp/UW/ngumbau - IPMH study"),
-                       machinetype = c("PC", "Mac", "Mac", "PC"))
+                                         "C:/Users/hp/UW/ngumbau - IPMH study",
+                                         "C:/Users/Olivia Schultes/OneDrive - UW/ngumbau's files - IPMH study"),
+                       machinetype = c("PC", "Mac", "Mac", "PC", "PC"))
 
 ### Defining OneDrive data filepath based on analyst ###
 current_wd <- getwd()
@@ -23,6 +25,8 @@ if (grepl("DAMARIS", current_wd)) {
     ipmh_filepath <- "/Users/yuweiwang_1997/Library/CloudStorage/OneDrive-UW/IPMH study" 
 }else if (grepl("hp", current_wd)) { 
     ipmh_filepath <- "C:/Users/hp/UW/ngumbau - IPMH study"
+} else if (grepl("Olivia Schultes", current_wd)) {
+    ipmh_filepath <- "C:/Users/Olivia Schultes/OneDrive - UW/ngumbau's files - IPMH study"
 } else {
  print("No filepath")
 }
