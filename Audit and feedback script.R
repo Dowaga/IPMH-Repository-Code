85983dc137c2eddc18d1f5b77cbf607998fe5648
David Owaga <owaga2007@gmail.com>
2025-11-27 11:47:03 +0300

    Third PDG Audit and feedback report

--- Audit and feedback script.R
+++ Audit and feedback script.R
@@ -38,19 +38,19 @@ rct_ppw <- rct_ppw %>%
 pm$pm_date <- as.Date(pm$pm_date)
 telepsych$telepsych_date <- as.Date(telepsych$tele_date)
 
-#time period for the data [2025-10-01, 2025-07-01]
+#time period for the data [2025-11-30, 2025-09-01]
 pm_all <- pm
 rct_ppw_all <- rct_ppw
-daily_closeout <- daily_closeout %>% filter(rct_dcr_date < "2025-10-01")
-daily_closeout <- daily_closeout %>% filter(rct_dcr_date >= "2025-07-01")
-phq2_gad2_abstract <- phq2_gad2_abstract %>% filter(screening_date < "2025-10-01")
-phq2_gad2_abstract <- phq2_gad2_abstract %>% filter(screening_date >= "2025-07-01")
-rct_ppw <- rct_ppw %>% filter(clt_timestamp <= "2025-10-01")
-rct_ppw <- rct_ppw %>% filter(clt_timestamp >= "2025-07-01")
-pm <- pm %>% filter(pm_date <= "2025-10-01")
-pm <- pm %>% filter(pm_date >= "2025-07-01")
-telepsych <- telepsych %>% filter(tele_date <= "2025-10-01")
-telepsych <- telepsych %>% filter(tele_date >= "2025-07-01")
+daily_closeout <- daily_closeout %>% filter(rct_dcr_date < "2025-11-30")
+daily_closeout <- daily_closeout %>% filter(rct_dcr_date >= "2025-09-01")
+phq2_gad2_abstract <- phq2_gad2_abstract %>% filter(screening_date < "2025-11-30")
+phq2_gad2_abstract <- phq2_gad2_abstract %>% filter(screening_date >= "2025-09-01")
+rct_ppw <- rct_ppw %>% filter(clt_timestamp <= "2025-11-30")
+rct_ppw <- rct_ppw %>% filter(clt_timestamp >= "2025-09-01")
+pm <- pm %>% filter(pm_date <= "2025-11-30")
+pm <- pm %>% filter(pm_date >= "2025-09-01")
+telepsych <- telepsych %>% filter(tele_date <= "2025-11-30")
+telepsych <- telepsych %>% filter(tele_date >= "2025-09-01")
 
 
 #uniform study site number for all datasets
@@ -290,7 +290,7 @@ pm_referral <- rct_ppw_int %>%
     filter(abs_phq_ref_pm == "Yes" | abs_gad7_ref_pm == "Yes") %>% 
     mutate(clt_date = ymd(clt_date)) %>%
     #filter referral based on audit period
-  filter(between(clt_date, ymd("2025-07-01"), ymd("2025-10-01")))
+  filter(between(clt_date, ymd("2025-09-01"), ymd("2025-11-30")))
 
 pm_referral <- pm_referral %>%
     mutate(day = floor_date(clt_timestamp, "day")) %>%
