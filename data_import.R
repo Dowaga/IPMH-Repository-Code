3491c3ab302db40250134da53fc03e91147bac2d
David Owaga <owaga2007@gmail.com>
2026-04-07 09:32:27 +0300

    data freeze

--- data_import.R
+++ data_import.R
@@ -8,13 +8,18 @@
 source("DataTeam_ipmh.R")
 source("Dependencies.R")
 source("REDCap_datapull.R")
-file_date <- fileSnapshot(file.path(ipmh_filepath, "/Data/6. RCT PPW data/"))
-file_date <- rownames(file_date$info[which.max(file_date$info$mtime),])
-file_date <- gsub("^.*?_2","2",file_date)
-file_date <- str_remove(file_date,".csv")
-file_date <- format(file_date, format="%d %B %Y")
 
-
+if (exists("data_freeze")) {
+    latest <- fileSnapshot(file.path(ipmh_filepath, "/Data/6. RCT PPW data/"))
+    latest <- rownames(latest$info[which.max(latest$info$mtime),])
+    latest <- as.Date(gsub(".*RCT_PPW_(.+)\\.csv", "\\1", latest))  # extracts YYYY-MM-DD
+    
+    file_date <- format(min(as.Date(data_freeze), latest), "%Y-%m-%d")  # keep YYYY-MM-DD
+} else {
+    latest <- fileSnapshot(file.path(ipmh_filepath, "/Data/6. RCT PPW data/"))
+    latest <- rownames(latest$info[which.max(latest$info$mtime),])
+    file_date <- gsub(".*RCT_PPW_(.+)\\.csv", "\\1", latest)  # YYYY-MM-DD string
+}
 
 ppw_rct_df <- read.csv(paste0(ipmh_filepath, "/Data/6. RCT PPW data/RCT_PPW_", 
                               file_date, ".csv", sep="")) %>% 
